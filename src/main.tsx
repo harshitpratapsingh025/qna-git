fcd86a6fc666fbda069540534868f29da23ea7d7
Harshit singh <harshit.singh@compassdigital.io>
2025-05-25 11:14:29 +0530

    Added fix for Typescript issues

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './app/App';
 import store from './app/store/store';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <Provider store={store}>
     <BrowserRouter>
